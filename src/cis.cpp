e4063634024ff80dab1046aa94819b2466feaa6b
Zedritsch <cedric.buttkus@gmail.com>
2022-08-24 17:17:43 +0200

    Added `cis.cpp` and excluded other Project Files

---
+++ src/cis.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+// @Zedritsch
+
+const char REPLACEMENTS[] = {'\t', '\n', ' '};
+const char BUILT_IN[] = {'"', '$', '\'', '(', ')', '*', ',', '.', ':', ';', '?', '@', '[', ']', '{', '}'};
+
+
+int main(const int ARGC, const char *ARGV[]) {
+	if (ARGC < 2)
+		return 0;
+
+	return 1;
+}
