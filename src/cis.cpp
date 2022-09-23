17f0ed43a3c211bff3609cb0b91c33780167ced4
Zedritsch <cedric.buttkus@gmail.com>
2022-09-23 18:26:43 +0200

    Added a Bit of Tests

--- src/cis.cpp
+++ src/cis.cpp
@@ -7,9 +7,55 @@ const char REPLACEMENTS[] = {'\t', '\n', ' '};
 const char BUILT_IN[] = {'"', '$', '\'', '(', ')', '*', ',', '.', ':', ';', '?', '@', '[', ']', '{', '}'};
 
 
-int main(const int ARGC, const char *ARGV[]) {
+// Function that add two numbers
+int add(int x, int y) {
+	return x + y;
+}
+
+// Function that multiplies two
+// numbers
+int multiply(int x, int y) {
+	return x * y;
+}
+
+// Function that takes a pointer
+// to a function
+int invoke(int x, int y, int (*func)(int, int)) {
+	return func(x, y);
+}
+
+// Driver Code
+int main() {
+	// Pass pointers to add & multiply
+	// function as required
+	std::cout << "Addition of 20 and 10 is ";
+	std::cout << invoke(20, 10, &add) << '\n';
+
+	std::cout << "Multiplication of 20" << " and 10 is ";
+	std::cout << invoke(20, 10, &multiply) << '\n';
+
+	return 0;
+}
+
+/*int main(const int ARGC, const char *ARGV[]) {
 	if (ARGC < 2)
 		return 0;
 
+	std::wifstream file(ARGV[1]);
+	if (file.fail())
+		return 0;
+
+	std::wstring str = L"😏😏😏😏😏";
+	std::wcout << str;
+	wchar_t ch = file.get();
+	while (ch != 65535) {
+		switch (ch) {
+			case L' ':
+				break;
+		}
+
+		ch = file.get();
+	}
+
 	return 1;
-}
+}*/
